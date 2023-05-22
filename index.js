5558877d033e59749cea2698909e9f2b64719377
Officialibn1 <officialibn001@gmail.com>
2023-05-22 11:51:01 +0100

    Added Javascript file

---
+++ index.js
@@ -0,0 +1,12 @@
+function toggleNavbar() {
+    var navbar = document.getElementById('navbar');
+    navbar.classList.toggle('open');
+}
+
+var openBtn = document.getElementById('open');
+
+openBtn.addEventListener('click', toggleNavbar);
+
+var closeBtn = document.getElementById('close');
+
+closeBtn.addEventListener('click', toggleNavbar);
